b006d1f9d2749437cb0f12ad5840ad8c372ac453
Tim Sonner <66705347+timsonner@users.noreply.github.com>
2023-02-27 23:35:23 -0800

    add webview

---
+++ BetaWebKitScanner/BetaWebKitScanner/WebView.swift
@@ -0,0 +1,59 @@
+//  WebView.swift
+//  WebviewScanner
+//
+//  Created by Tim Sonner on 2/26/23.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let urlString: String
+    @Binding var statusCode: Int?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        webView.navigationDelegate = context.coordinator
+        if let url = URL(string: urlString) {
+            webView.load(URLRequest(url: url))
+        }
+        return webView
+    }
+    
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        if let url = URL(string: urlString) {
+            webView.load(URLRequest(url: url))
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+    
+    class Coordinator: NSObject, WKNavigationDelegate {
+        var parent: WebView
+        
+        init(parent: WebView) {
+            self.parent = parent
+        }
+        
+        func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+            if let httpResponse = (navigationResponse.response as? HTTPURLResponse) {
+                DispatchQueue.main.async {
+                    self.parent.statusCode = httpResponse.statusCode
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self.parent.statusCode = 404
+                }
+            }
+            decisionHandler(.allow)
+        }
+        
+        func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+            DispatchQueue.main.async {
+                self.parent.statusCode = (error as NSError).code
+            }
+        }
+    }
+}
